1c5149fe34214a1a8d1f91b6fddb3b59fe3a8e62
Kev <kevin.estrada1@udea.edu.co>
2024-09-30 22:20:48 -0500

    Primera Versión del Laboratorio

---
+++ src/main/java/com/udea/Lab2Despliegue/Lab2DespliegueApplication.java
@@ -0,0 +1,13 @@
+package com.udea.Lab2Despliegue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2DespliegueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2DespliegueApplication.class, args);
+	}
+
+}
